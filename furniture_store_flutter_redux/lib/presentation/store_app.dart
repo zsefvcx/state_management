c53743d8a611be23b1e89524a68efbb68ad4b2ae
zsefvcx <zsefvcx@list.ru>
2023-06-18 18:10:46 +0300

    lesson13 furniture_store flutter redux 6%

--- furniture_store_flutter_redux/lib/presentation/store_app.dart
+++ furniture_store_flutter_redux/lib/presentation/store_app.dart
@@ -7,7 +7,7 @@ import 'package:furniture_store/presentation/route_generator.dart';
 // final mainBlocProvider = StateNotifierProvider<MainBloc, MainStatus>((ref) => BlocFactory.instance.get<MainBloc>());
 // final favoritesProvider = StateNotifierProvider<FavoritesBloc ,Favorites>((ref) => BlocFactory.instance.get<FavoritesBloc>());
 // final shoppingBasketProvider = StateNotifierProvider<ShoppingBasketBloc, ShoppingBasket>((ref) => BlocFactory.instance.get<ShoppingBasketBloc>());
-
+//https://habr.com/ru/articles/481624/
 enum Actions {
   bas,
   addBas,
@@ -16,12 +16,11 @@ enum Actions {
   remAll,
   getCountBas,
   statusBas,
-
 }
 
-// int shoppingBasketReducer(int state, dynamic action) {
-//   return action == Actions.Increment ? state + 1 : state;
-// }
+ int shoppingBasketReducer(int state, dynamic action) {
+   return action == Actions.Increment ? state + 1 : state;
+ }
 
 class StoreApp extends StatelessWidget {
   const StoreApp({super.key});
